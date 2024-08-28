0b47072764ee8d2c33b3843fb35fe6ed9c2a69a9
SebastianSchult <155525668+SebastianSchult@users.noreply.github.com>
2024-08-28 12:10:13 +0200

    card animation and card logic

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,6 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { Game } from '../../models/game';
 
 @Component({
   selector: 'app-game',
@@ -9,5 +10,28 @@ import { Component } from '@angular/core';
   styleUrl: './game.component.scss'
 })
 export class GameComponent {
+  pickCardAnimation = false;
+  currentCard: string | undefined = '';
+  game: Game;
 
+  constructor() {
+    this.game = new Game();
+  }
+
+  ngOnInit():void {
+    this.newGame();
+  }
+
+  newGame() {
+    this.game = new Game();
+    console.log(this.game);
+  }
+
+  takeCard() {
+    this.currentCard = this.game.stack.pop();
+    console.log('take card');
+    console.log(this.currentCard);
+    this.pickCardAnimation = true;
+  }
+  
 }
---
+++ src/models/game.ts
@@ -0,0 +1,35 @@
+export class Game {
+    public players: string[] = [];
+    public stack: string[] = [];
+    public playedCards: string[] = [];
+    public currentPlayer: number = 0;
+  
+    constructor() {
+      for (let i = 1; i < 14; i++) {
+        this.stack.push('ace_' + i);
+        this.stack.push('clubs_' + i);
+        this.stack.push('diamonds_' + i);
+        this.stack.push('hearts_' + i);
+      }
+  
+      this.shuffle(this.stack);
+    }
+  
+    shuffle(array: string[]): void { // Typangabe für den Parameter hinzugefügt
+      let currentIndex = array.length;
+  
+      // Solange noch Elemente zum Mischen übrig sind...
+      while (currentIndex != 0) {
+        // Wähle ein verbleibendes Element...
+        let randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+  
+        // Und tausche es mit dem aktuellen Element.
+        [array[currentIndex], array[randomIndex]] = [
+          array[randomIndex],
+          array[currentIndex],
+        ];
+      }
+    }
+  }
+  
